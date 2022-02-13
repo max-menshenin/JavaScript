8aba38a38a7540380404b8bb9cf3bcc4284dc56d
Max Menshenin <max.menshenin.dev@yandex.ru>
2022-02-13 14:20:52 +0300

    minor changes

--- Task_2/script.js
+++ Task_2/script.js
@@ -1,4 +1,5 @@
 /*Напишите функцию, которая принимает в качестве аргументов строку и объект, а затем проверяет, есть ли у переданного объекта свойство с данным именем. Функция должна возвращать true или false.*/
+
 function hasFunc(str, obj) {
     if (obj.hasOwnProperty(str)) return true;
     else false;
@@ -6,6 +7,5 @@ function hasFunc(str, obj) {
 const obj = {
     a: 'hello',
 };
-//console.log(hasFunc('a', obj));
 
 console.log(obj.hasOwnProperty('a'));
