00ce2caefe46e2ce018cb74ac3c94528c7689312
Max Menshenin <max.menshenin.dev@yandex.ru>
2022-02-13 19:28:43 +0300

    Optimized

--- Task_2/script.js
+++ Task_2/script.js
@@ -1,11 +1,8 @@
 /*Напишите функцию, которая принимает в качестве аргументов строку и объект, а затем проверяет, есть ли у переданного объекта свойство с данным именем. Функция должна возвращать true или false.*/
 
-function hasFunc(str, obj) {
-    if (obj.hasOwnProperty(str)) return true;
-    else false;
-}
 const obj = {
-    a: 'hello',
+    a: 'hat',
 };
-
-console.log(obj.hasOwnProperty('a'));
+let str = 'a';
+const hasFunc = (str, obj) => obj.hasOwnProperty(str);
+console.log(obj.hasOwnProperty(str));
