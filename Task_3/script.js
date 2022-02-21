8f5a3baa08762bb98e174ef73a5864314ec7f137
Postmind <53375202+max-menshenin@users.noreply.github.com>
2022-02-21 17:56:26 +0300

    Delete Task_3 directory

--- Task_3/script.js
+++
@@ -1,8 +0,0 @@
-/* Напишите функцию, которая создает пустой объект, но без прототипа. */
-
-function createObj() {
-	return Object.create(null);
-}
-sampleObj = createObj();
-
-console.log(Object.getPrototypeOf(sampleObj));
